40c3e481884fc1497b86049c2ad4c9227210588b
Erika Tavera <dugarerika@gmail.com>
2020-11-11 20:45:08 +0300

    Instalo mongoose y creo archivo de conexion

--- nodepop/routes/index.js
+++ nodepop/routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var createError = require('http-errors');
+// Object Destructuring
+const {query, validationResult} = require('express-validator');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+// recibir parametros en query string
+router.get('/queryString', (req, res, next) => {
+  console.log(req.query);
+  res.send('ok');
+});
+
+// recibir parametros en el body
 
 module.exports = router;
--- nodepop/routes/users.js
+++ nodepop/routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var createError = require('http-errors');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {

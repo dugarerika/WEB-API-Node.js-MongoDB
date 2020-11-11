6b46aa82ef943d08e1657695d5f593a571fdf8bb
Erika Tavera <dugarerika@gmail.com>
2020-11-11 18:16:58 +0300

    cambio motor de vistas

--- nodepop/app.js
+++ nodepop/app.js
@@ -8,8 +8,12 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'html');
+app.engine('html', require('ejs').__express);
 
+app.locals.title = 'Nodepop';
+
+//  middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
